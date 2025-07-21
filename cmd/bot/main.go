03fe846cff490a35ca3c8d41c74c8ad4d467e124
filinan <filin.aa@phystech.edu>
2025-07-21 19:52:05 +0300

    вывод токена

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	_ = godotenv.Load() // локально читает .env, Railway это игнорирует.
+	for _, e := range os.Environ() {
+		log.Println("ENV:", e)
+	}
 
 	token := os.Getenv("TELEGRAM_TOKEN")
 	if token == "" {
 		log.Fatal("TELEGRAM_TOKEN не найден")
 	}
-
-	log.Println("TOKEN успешно загружен:", token[:5]) // выводим часть токена для проверки
 }
